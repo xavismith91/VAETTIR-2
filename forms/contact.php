3ea1068ddce4f408ab090f0b860ae0570cd66770
Xavier Solano <89545518+xavismith91@users.noreply.github.com>
2023-09-26 12:12:33 -0600

    correo

--- forms/contact.php
+++ forms/contact.php
@@ -2,8 +2,8 @@
 
     $nombre =$_POST["nombre"];
     $mensaje=$_POST["message"];
-    $correo = $_POST["correo"]; //correo donde va a llegar los datos del formulario
-    $correo2 = $_POST["email"]; //correo del cliente 
+    $correo = $_POST["correo"]; // correo que ingresan en el formulario
+    $correo2 = 'sistemas@ce2000.mx' // correo en donde van a llegar los datos del formulario 
     $telefono = $_POST['telefono'];
     $tiposervicio = $_POST['servicio'];
     $remitente= "VAETTIR";
